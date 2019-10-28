b931a33ede6f66aa021cbc040db223b3c11b4005
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2019-10-28 14:38:11 +0100

    Initial

--- renderer.js
+++ renderer.js
@@ -4,3 +4,42 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const audioCtx = new window.AudioContext();
+const audioElement = document.querySelector('audio');
+
+const streamNode = audioCtx.createMediaStreamDestination();
+const gainNode = audioCtx.createGain();
+
+const pannerOptions = { pan: 0 };
+const panner = new StereoPannerNode(audioCtx, pannerOptions);
+
+const { getAudioBuffer } = require('electron-webaudio-decode');
+
+
+function play(audiobuf) {
+    var source = audioCtx.createBufferSource();
+    source.buffer = audiobuf;
+
+    source.connect(gainNode).connect(panner).connect(streamNode);
+    audioElement.controls = true;
+    audioElement.srcObject = streamNode.stream;
+
+    source.start(0);
+}
+
+const volumeControl = document.querySelector('#volume');
+
+const pannerControl = document.querySelector('#panner');
+
+pannerControl.addEventListener('input', function() {
+    panner.pan.value = this.value;
+}, false);
+
+volumeControl.addEventListener('input', function() {
+    gainNode.gain.value = this.value;
+}, false);
+
+getAudioBuffer("./6771641_Sun_Is_Shining_Extended.aiff").then(function(audioBuffer) {
+    play(audioBuffer);
+});
